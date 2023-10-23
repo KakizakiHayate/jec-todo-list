9e9a4db41b940c57c0737b08bd85219880eebff3
kakizaki hayate <hayate.h.0704@gmail.com>
2023-10-23 22:43:46 +0900

    データベースに追加できるようになった

--- App/Model/TodoModel.php
+++ App/Model/TodoModel.php
@@ -1,25 +1,45 @@
 <?php
 
 namespace Model;
+use mysqli;
+
 class TodoModel
 {
     private $db;
 
-    public function __contruct()
+    public function __construct()
     {
-        $host = 'localhost';
-        $user = 'user1';
+        // TODO user = admin, pass = adminpass,でやると失敗する
+        $host = 'mysql';
+        $user = 'root';
         $pass = 'rootpassword';
         $dbName = 'TodoListDB';
         $this->db = new mysqli($host, $user, $pass, $dbName);
-        if ($this->db->connect_error) {
-            die('データベース接続エラー: ' . $this->db->connect_error);
+
+        if (mysqli_connect_errno()) {
+            echo('データベース接続エラー: ' . mysqli_connect_errno());
+            exit();
         }
+        echo("Initial character set: %s\n" . $this->db->character_set_name());
+        if (!$this->db->set_charset("utf8")) {
+            echo("Error loading character set utf8: %s\n" . $this->db->error);
+            exit();
+        } else {
+            echo("Current character set: %s\n" . $this->db->character_set_name());
+        }
+
     }
 
     public function createTasks($overview, $detail, $limitDate, $assigner)
     {
-
+        $sql = "INSERT INTO tasks (overview, detail, limit_date, assigner_name, is_deleted, created_at, updated_at)
+                VALUES ('$overview', '$detail', '$limitDate', '$assigner', 0, NOW(), NOW());";
+        echo $sql;
+        if ($this->db->query($sql) === true) {
+            echo 'タスクが正常に登録されました。';
+        } else {
+            echo 'タスクの登録中に問題が発生しました。';
+        }
     }
 
     public function fetchTasks()
