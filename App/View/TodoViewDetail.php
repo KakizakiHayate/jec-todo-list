165cdfbcd08ed4181a359aaf495f758fec2a214f
kakizaki hayate <hayate.h.0704@gmail.com>
2023-11-05 03:16:39 +0900

    詳細ページ追加

---
+++ App/View/TodoViewDetail.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/reset.css" rel="stylesheet" />
+    <link href="css/todo-view-style.css" rel="stylesheet" />
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
